f5b9575dccf14e25ccb953480229070a0d9ccd5a
Ryan Ghaida <ryaboug@gmail.com>
2019-10-02 15:33:59 -0400

    migrated texts

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_173034) do
+ActiveRecord::Schema.define(version: 2019_10_02_192939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "texts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
