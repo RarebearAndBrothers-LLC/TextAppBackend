61fe0651b4cc6ae227976ff28f124af3f2eeec25
Ryan Ghaida <ryaboug@gmail.com>
2019-10-05 22:20:32 -0400

    trying to clone on new machine

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,6 @@
 class AuthenticationController < ApplicationController
     skip_before_action :authenticate_request
-   #The authenticate action will take the JSON parameters for email and password through the params hash and pass them to the AuthenticateUser command. If the command succeeds, it will send the JWT token back to the user.
+   #The authenticate action will take the JSON parameters for email, password and cell through the params hash and pass them to the AuthenticateUser command. If the command succeeds, it will send the JWT token back to the user.
     def authenticate
       command = AuthenticateUser.call(params[:email], params[:password], params[:cell])
    
