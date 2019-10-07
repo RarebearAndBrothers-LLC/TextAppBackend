5a44f8ca87328bff10f8ff922531e1ad1fdd8cab
Ryan Ghaida <ryaboug@gmail.com>
2019-10-07 12:35:17 -0400

    adding sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::API
     #By using before_action, the server passes the request headers (using the built-in object property request.headers) to AuthorizeApiRequest every time the user makes a request. Calling result on AuthorizeApiRequest.call(request.headers) is coming from SimpleCommand module where it is defined as attr_reader :result. The request results are returned to the @current_user, thus becoming available to all controllers inheriting from ApplicationController.
     before_action :authenticate_request
      attr_reader :current_user
+    #  helper_method :current_user
+
+    #   def current_user
+    #     if session[:user_id]
+    #       @current_user ||= User.find(session[:user_id])
+    #     else
+    #       @current_user = nil
+    #     end
+    #   end
    
      private
    
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :todos
   resources :texts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
